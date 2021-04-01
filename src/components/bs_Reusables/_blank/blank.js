d56fd74504235ab28d522ccfbc3fc43cc131fb1a
gauravkeerthi <gauravkeerthi@gmail.com>
2021-04-01 08:19:37 +0800

    added blank

---
+++ src/components/bs_Reusables/_blank/blank.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class XXXXX extends Component {
+  render() {
+    return (
+      
+        <>
+            Component
+        </>
+        
+      
+      
+    )
+  }
+}
+
+export default XXXXX;
